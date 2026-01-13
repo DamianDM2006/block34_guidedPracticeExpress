3f37579a6fe876b6d8ab041d7e95817bd6edd8e1
Damian Martinez <DCAM.DAMIAN.DM@GMAIL.COM>
2026-01-12 20:05:23 -0700

    Initialize solution

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+export default app;
+
+import playlist from "#db/playlist";
+
+app.get("/", (req, res) => {
+  res.send("You've reached the Playlist API!");
+});
+
+app.get("/playlist", (req, res) => {
+  res.send(playlist);
+});
+
+app.get("/playlist/:index", (req, res) => {
+  const { index } = req.params;
+  const song = playlist[index];
+
+  // We have to `return` in this guard case to avoid sending multiple responses!
+  if (!song)
+    return res.status(404).send("That song does not exist in the playlist.");
+
+  res.send(song);
+});
---
+++ db/playlist.js
@@ -0,0 +1,9 @@
+const playlist = [
+  "Exhilarating Lights",
+  "Space for Disco",
+  "Warm Heart, Cold Dinner",
+  "Sound of Voices",
+  "Tearful Paradise",
+];
+
+export default playlist;
